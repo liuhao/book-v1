a240bb42ed1587c39b52a4edf695121ee9ecfa3e
Liu Hao <hao.liu@fulansoft.com>
2012-07-31 18:03:07 +0800

    add cookie servlet code

---
+++ src/main/java/com/coreservlets/book/cookie/RegistrationServlet.java
@@ -0,0 +1,31 @@
+package com.coreservlets.book.cookie;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * Reads firstName and lastName request parameters and forwards
+ * to JSP page to display them. Uses session-based bean sharing
+ * to remember previous values.
+ */
+
+public class RegistrationServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        NameBean nameBean =
+                (NameBean) session.getAttribute("nameBean");
+        if (nameBean == null) {
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            nameBean = new NameBean(firstName, lastName);
+            session.setAttribute("nameBean", nameBean);
+        }
+        String address = "/WEB-INF/mvc-sharing/ShowName.jsp";
+        RequestDispatcher dispatcher =
+                request.getRequestDispatcher(address);
+        dispatcher.forward(request, response);
+    }
+}
